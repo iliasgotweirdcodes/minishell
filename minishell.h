d14e3e52fca210008bc046eab9985f7fbac7de91
ilel-hla <iliaselhal1@gmail.com>
2025-04-21 15:08:31 +0100

    demo

---
+++ minishell.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilel-hla <ilel-hla@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/21 15:03:56 by ilel-hla          #+#    #+#             */
+/*   Updated: 2025/04/21 15:08:04 by ilel-hla         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/wait.h>
+# include <fcntl.h>
+# include <signal.h>
+
+
+#endif
