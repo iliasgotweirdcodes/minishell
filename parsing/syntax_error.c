1c4815a2743e3d9ea336dc8f9e1435512e149d53
ilel-hla <iliaselhal1@gmail.com>
2025-04-23 22:58:24 +0100

    here doc needs to be done

--- parsing/syntax_error.c
+++ parsing/syntax_error.c
@@ -6,7 +6,7 @@
 /*   By: ilel-hla <ilel-hla@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 22:58:22 by ilel-hla          #+#    #+#             */
-/*   Updated: 2025/04/23 22:47:23 by ilel-hla         ###   ########.fr       */
+/*   Updated: 2025/04/23 22:57:55 by ilel-hla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,7 @@ int	validate_syntax(t_token *tokens)
     {
          if (current->type == HEREDOC && current->next && current->next->type == WORD)
         {
-            printf("OPRN HEREDOC HEREEEEEE\n");
+           printf("OPEN HEREDOC HEREEEEEE\n");
             if (current->next && current->next->type == WORD)
             {
                 prev = current->next;
@@ -51,7 +51,7 @@ int	validate_syntax(t_token *tokens)
             }
             else
             {
-                if (current->next)
+               if (current->next)
                     print_syntax_error(current->next);
                 else
                     print_syntax_error(current);
