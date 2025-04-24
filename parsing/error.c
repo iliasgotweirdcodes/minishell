ddcd1bee789cf3b41cc70cef4987560f1efa4f9b
Abdelilah Romani <aromani@e3r3p13.1337.ma>
2025-04-24 22:19:27 +0100

    new

--- parsing/error.c
+++ parsing/error.c
@@ -6,7 +6,7 @@
 /*   By: aromani <aromani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 23:17:04 by ilel-hla          #+#    #+#             */
-/*   Updated: 2025/04/24 21:37:53 by aromani          ###   ########.fr       */
+/*   Updated: 2025/04/24 21:40:43 by aromani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ void ft_error(char *msg)
 {
 	write(2, msg, ft_strlen(msg));
 }
-
 
 void	print_quote_error(void)
 {
